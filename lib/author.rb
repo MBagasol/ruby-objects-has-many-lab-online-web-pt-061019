c6c32423c15af5e5ed7e76860070fdadc9adc54f
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-15 02:39:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,36 @@
-class Author 
-  attr_accessor :name, :posts
-  
 
-  def initialize(name)
-    @name = name
+class Author 
+  attr_accessor :title, :posts
+  
+  @@post_count = 1
+  def initialize(titls)
+    @title = title
     @posts = []
   end
   
-  def posts 
-    @posts 
+  def post 
+    @post 
   end
   
- 
-  
-  
-  
-  
-  
+  def add_post(post) 
+    @posts << post
+    @@post_count +=1
+    post.artist = self 
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
 end
